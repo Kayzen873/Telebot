a1bc9281673b897562410489663a4e0cbf51c57f
Kayzen873 <55250944+Kayzen873@users.noreply.github.com>
2023-06-23 01:17:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
 global.BOT_TOKEN = "5990974204:AAF1BQ4dt0l9GasjeOtGLR8XXlFKKYXq-qY" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
+global.BOT_NAME = "Valkyriezen BOT" //your bot name
+global.OWNER_NAME = "Zen" //your name
 global.OWNER_NUMBER = "6283179799943" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER = ["https://t.me/Kayzen1337", "https://t.me/Kayzen1337"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
